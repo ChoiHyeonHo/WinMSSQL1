5548e04e71a123e9e82ef165a46a2b2e0ec1fb20
최현호 <hyunho1232@naver.com>
2020-11-24 15:48:06 +0900

    초기 커밋.

---
+++ MSSQL_DAC1/ProductDB.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using MSSQL_VO1;
+
+namespace MSSQL_DAC1
+{
+    public class ProductDB
+    {
+        public DataTable GetProductList()
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string sql = @"select P.ProductID, P.ProductName, S.CompanyName, C.CategoryName, QuantityPerUnit
+  from Products P inner join Suppliers S on P.SupplierID = S.SupplierID
+        inner join Categories C on P.CategoryID = C.CategoryID";
+
+                using (SqlDataAdapter da = new SqlDataAdapter(sql, conn))
+                {                    
+                    da.Fill(dt);
+                }
+            }
+
+            return dt;
+        }
+
+        public List<ProductInfoVO> GetProductList2()
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            List<ProductInfoVO> list = new List<ProductInfoVO>();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string sql = @"select P.ProductID, P.ProductName, S.CompanyName, C.CategoryName, QuantityPerUnit
+  from Products P inner join Suppliers S on P.SupplierID = S.SupplierID
+        inner join Categories C on P.CategoryID = C.CategoryID";
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    list = Helper.DataReaderMapToList<ProductInfoVO>(reader);
+                    //while (reader.Read())
+                    //{
+                    //    ProductInfoVO vo = new ProductInfoVO
+                    //    {
+                    //        ProductID = Convert.ToInt32(reader["ProductID"]),
+                    //        ProductName = reader[1].ToString(),
+                    //        CategoryName = reader["CategoryName"].ToString(),
+                    //        CompanyName = reader["CompanyName"].ToString(),
+                    //        QuantityPerUnit = reader["QuantityPerUnit"].ToString()
+                    //    };
+
+                    //    list.Add(vo);
+                    //}
+                    conn.Close();
+                }
+            }
+
+            return list;
+        }
+
+        public DataTable GetCategory()
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string sql = @"select CategoryID, CategoryName from Categories";
+
+                using (SqlDataAdapter da = new SqlDataAdapter(sql, conn))
+                {
+                    da.Fill(dt);
+                }
+            }
+
+            return dt;
+        }
+
+        public List<ComboItemVO> GetCodeList()
+        {
+            List<ComboItemVO> list = new List<ComboItemVO>();
+
+            string strConn = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string sql = @"select Code, CodeName, Gubun from VW_GetCode";
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    list = Helper.DataReaderMapToList<ComboItemVO>(reader);
+
+                    //while (reader.Read())
+                    //{
+                    //    ComboItemVO vo = new ComboItemVO
+                    //    {
+                    //        Code = reader["Code"].ToString(),
+                    //        CodeName = reader["CodeName"].ToString(),
+                    //        Gubun = reader["Gubun"].ToString()
+                    //    };
+
+                    //    list.Add(vo);
+                    //}
+                    conn.Close();
+                }
+            }
+
+            return list;
+        }
+
+        public List<RegionVO> GetRegionList()
+        {
+            List<RegionVO> list = new List<RegionVO>();
+
+            string strConn = ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string sql = @"select TerritoryID, TerritoryDescription, 
+                                        cast(T.RegionID as varchar) RegionID, TerritoryDescription RegionDesc
+                                        from Territories T inner join Region R on T.RegionID = R.RegionID";
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    list = Helper.DataReaderMapToList<RegionVO>(reader);
+                    conn.Close();
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ WinMSSQL1/Form2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WinMSSQL1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 41);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "List";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "DataTable";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "배",
+            "사과",
+            "딸기"});
+            this.comboBox2.Location = new System.Drawing.Point(601, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(165, 20);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(508, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Region";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(416, 415);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(601, 70);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(165, 20);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 12);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Categories";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
